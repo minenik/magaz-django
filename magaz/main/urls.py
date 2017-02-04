2f69051f5b4b2bdafa8bf76c8bc94a558067a4dd
minenik <petrovpf286@yandex.ru>
2017-02-04 20:45:32 +0300

    p

--- magaz/main/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import *
-from blog.views import home, item, index
-
-urlpatterns = [
-    
-    url(r'^admin/', include(admin.site.urls))
-    url(r'^$', home),
-    url(r'^item/(?P<alias>[^/]+)', item),
-]
---
+++ myshop/shop/urls.py
@@ -0,0 +1,15 @@
+from django.conf.urls import url
+from . import views
+from django.conf import settings
+from django.conf.urls.static import static
+
+
+urlpatterns = [
+    url(r'^$', views.ProductList, name='ProductList'),
+    url(r'^(?P<category_slug>[-\w]+)/$', views.ProductList, name='ProductListByCategory'),
+    url(r'^(?P<id>\d+)/(?P<slug>[-\w]+)/$', views.ProductDetail, name='ProductDetail'),
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) 
+
