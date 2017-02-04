b1750eee27b5831030fd64cc382832356857adb7
minenik <petrovpf286@yandex.ru>
2017-02-04 20:55:24 +0300

    p

--- myshop/shop/admin.py
+++ myshop/shop/admin.py
@@ -1,11 +1,11 @@
 from django.contrib import admin
 from .models import Category, Product
 
-# Модель категории
+
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ['name', 'slug']
     prepopulated_fields = {'slug': ('name', )}
-# Модель товара
+
 
 class ProductAdmin(admin.ModelAdmin):
     list_display = ['name', 'slug', 'price', 'stock', 'available', 'created', 'updated']
