08e81e9ab36184be7c87cfad971c76560ca0038a
minenik <petrovpf286@yandex.ru>
2017-02-05 12:41:24 +0300

    zakaz

--- myshop2/myshop2/urls.py
+++ myshop2/myshop2/urls.py
@@ -18,5 +18,6 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^', include('shop.urls', namespace='shop'))
+    url(r'^', include('shop.urls', namespace='shop')),
+    url(r'^order/', include('orders.urls', namespace='orders')),
 ]
